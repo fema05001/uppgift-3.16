86a17a1c16b6323b80a68c2fd0ccc3643db0ef40
longf <longf@DESKTOP-2GJ1G8J>
2022-09-29 20:21:25 +0200

    Add project files.

---
+++ uppgift 3.16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift_3_16
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange antal minuter i låten");
+            string x=Console.ReadLine();
+           int a=int.Parse(x);
+            Console.WriteLine("ange antal sekunder i låten");
+            string y=Console.ReadLine();   
+            int b=int.Parse(y);
+            if (a >= 4 && b > 20) 
+            {
+                Console.WriteLine("Din låt får inte spelas på radiostationen");
+            }
+            else if (a <= 2 && b < 45)
+            {
+                Console.WriteLine("Din låt får inte spelas på radiostationen");
+            }
+            else
+            {
+                Console.WriteLine("Din låt får spelas på radiostationen");
+            }
+        }
+    }
+}
